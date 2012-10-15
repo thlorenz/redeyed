6d588d5bd3bfbf52169c9c8842af0d75be1f3a73
Thorsten Lorenz <thlorenz@gmx.de>
2012-10-15 07:39:20 -0400

    simple surround working

--- redeyed.js
+++ redeyed.js
@@ -1,14 +1,9 @@
 'use strict';
+/*jshint laxbreak: true */
 
-var syntax      =  require('./syntax')
-  , statements  =  syntax.statements
-  , expressions =  syntax.expressions
-  , values      =  syntax.values
-  , names       =  syntax.names
-  , util        =  require('util')
-  , toString    =  Object.prototype.toString
-  , padspaces   =  '                                        '
-  , padlines    =  '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
+var esprima  =  require('esprima')
+  , util     =  require('util')
+  , toString =  Object.prototype.toString
   ;
 
 function isString (obj) {
@@ -19,125 +14,76 @@ function isNumber (obj) {
   return toString.call(obj) == '[object Number]';
 }
 
-function stringifyBinaryExpression (node) {
-  // Location of operator is not reported, so we need to use locations of left and right literals
-  // in order to deduce the original padding around it.
-  // If padding on left was different from padding on right, we equalize it since we wouldn't know
-  // which side had the larger padding.
-  
-  // TODO: handle multi line expressions
-  var left           =  node.left
-    , right          =  node.right
-    , operator       =  node.operator
-    , leftlen        =  left.end.offset - left.start.offset
-    , rightlen       =  right.end.offset - right.start.offset
-    , totallen       =  right.end.offset - left.start.offset
-    , operatorlen    =  operator.length
-    , missing        =  totallen - (leftlen + operatorlen + rightlen)
-    , padsize        =  Math.floor(missing / 2)
-    , spaces
-    , paddedOperator
-    ;
-  
-  if (padsize > 0) {
-    spaces         =  padspaces.slice(-padsize);
-    paddedOperator =  spaces + operator + spaces;
-  } else {
-    paddedOperator = operator;
+function redeyed (code, opts) {
+  var parsed = esprima.parse(code, { tokens: true, range: true })
+    , tokens = parsed.tokens
+    , lastSplitEnd = 0
+    , splits = [];
+
+  function addSplit (start, end, before, after) {
+    if (start >= end) return;
+    if (before && after)
+      splits.push(before + code.slice(start, end) + after);
+    else
+      splits.push(code.slice(start, end));
+
+    lastSplitEnd = end;
   }
 
-  return left.value + paddedOperator + right.value;
-}
+  console.log(tokens);
 
-function redeyed (ast, opts) {
-  var acc = []
-    , match
-    , prevline = 1
-    , prevcol = 0;
+  tokens.forEach(function (token) {
+    var surroundForType = opts[token.type]
+      , surroundDefault = opts.Default
+      , surround
+      , surroundBefore
+      , surroundAfter 
+      , start
+      , end;
+     
+    if (surroundForType) {
+      surround = surroundForType[token.value] || surroundForType;
 
-  console.log(util.inspect(ast.body, false, 6, true));
+      start = token.range[0];
+      end = token.range[1] + 1;
 
-  function addText(value, node, parent) {
-    // Conserve white space
-    var padlinesLen
-      , padcolsLen
-      , padded;
+      surroundBefore = surround._before || surroundForType._before || opts._before || '';
+      surroundAfter  = surround._after  || surroundForType._after  || opts._after  || '';
 
-    if (isString(value) || isNumber(value)) {
-      padded = value;
-    } else {
-      if (value === null) padded = 'null';
+      addSplit(lastSplitEnd, start - 1);
+      addSplit(start, end, surroundBefore, surroundAfter);
     }
-    padlinesLen = node.start.line - prevline;
+  });
 
-    if (padlinesLen) {
-      padded = padded + padlines.slice(-padlinesLen);
-      padcolsLen = node.start.column;
-    } else {
-      padcolsLen = node.start.column - prevcol;
-    } 
-
-    prevcol = node.end.column;
-
-    padded = padcolsLen ? padspaces.slice(-padcolsLen) + padded : padded;
-
-    return acc.push(padded);
+  if (lastSplitEnd < code.length) {
+    addSplit(lastSplitEnd, code.length);
   }
 
-
-  (function walk (node, parent) {
-    var tokenval
-      , child
-      , nowalk = [ 'type', 'start', 'end' ];
-
-    //if (node.type) console.log(' ====> ', node.type);
-
-    if (isString(node)) {
-      return addText(node, parent);
-    }
-
-    tokenval = node.type && expressions[node.type];
-    if (tokenval) {
-      return addText(tokenval, node);
-    }
-
-    if (node.type && node.type === 'Literal') {
-      return addText(node.value, node);
-    }
-
-    if (node.type && node.type === 'BinaryExpression') {
-      return addText(stringifyBinaryExpression(node), parent);
-    }
-
-    if (Array.isArray(node)) {
-      return node.forEach(function (n) {
-        walk(n, node);
-      });
-    }
-
-    Object.keys(node).forEach(function (key) {
-      if (nowalk.indexOf(key) < 0) {
-      //  console.log('walking', key);
-        walk(node[key], node);
-      }
-    });
-
-  })(ast, undefined);
-  return acc.join('');
+  return splits.join('');
 }
 
-module.exports = redeyed;
+// '\u001b[36m', '\u001b[39m'
+
+var opts = {
+    Keyword: {
+        //'function' :  { before :  '__' ,  after :  '++' }
+        'function' :  { _before :  '\u001b[36m' }
+      , _before :  '\u001b[32m'
+    }
+  , _before: ''
+  , _after :  '\u001b[39m' 
+
+};
 
 if (module.parent) return;
 
-var acorn = require('acorn').parse;
+console.log(
+  redeyed('' + 
 
-function roundTripped (code) {
-  var ast = acorn(code, { linePositions: true, sourceFile: true });
-  return redeyed(ast);
-}
+function foo () {
+  return true;
+} 
 
-console.log('"' +
-  roundTripped('1 * 2  + 3') +
-  '"'
++ '', opts)
 );
+
