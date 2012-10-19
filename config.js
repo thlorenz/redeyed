b1b515aaac92313ad77d3669a5c837adc0352f02
Thorsten Lorenz <thlorenz@gmx.de>
2012-10-19 08:44:22 -0400

    reformat config

--- config.js
+++ config.js
@@ -5,48 +5,63 @@
  */
 
 module.exports = {
+
     'Boolean': {
       'true'   :  undefined
     , 'false'  :  undefined
     , _default :  undefined
     }
+
   , 'Identifier': {
       _default: undefined
     }
+
   , 'Null': {
       _default: undefined
     }
+
   , 'Numeric': {
       _default: undefined
     }
+
   , 'String': {
       _default: undefined
     }
+
   , 'Keyword': {
       'break'       :  undefined
-    , 'debugger'    :  undefined
-    , 'default'     :  undefined
-    , 'delete'      :  undefined
+
     , 'case'        :  undefined
     , 'catch'       :  undefined
     , 'continue'    :  undefined
+
+    , 'debugger'    :  undefined
+    , 'default'     :  undefined
+    , 'delete'      :  undefined
     , 'do'          :  undefined
+
     , 'else'        :  undefined
+
     , 'finally'     :  undefined
     , 'for'         :  undefined
     , 'function'    :  undefined
+
     , 'if'          :  undefined
     , 'in'          :  undefined
     , 'instanceof'  :  undefined
+
     , 'new'         :  undefined
     , 'return'      :  undefined
     , 'switch'      :  undefined
+
     , 'this'        :  undefined
     , 'throw'       :  undefined
     , 'try'         :  undefined
     , 'typeof'      :  undefined
+
     , 'var'         :  undefined
     , 'void'        :  undefined
+
     , 'while'       :  undefined
     , 'with'        :  undefined
     , _default      :  undefined
@@ -106,4 +121,6 @@ module.exports = {
 
     , _default: undefined
   }
+
+  , _default: undefined
 };
